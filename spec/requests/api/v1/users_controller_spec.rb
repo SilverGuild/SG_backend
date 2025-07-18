e3ebbce35a0dcd73a1a659ce54b315b2d8a4044c
elysableu <elysabward@gmail.com>
2025-07-18 17:20:22 -0600

    Update backend setup and start tests for users controller

---
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe UsersController do
+  xit "should retrieve all users" do
+
+  end
+
+  xit "should create a new user" do
+
+  end
+
+  xit "should retrieve one user" do
+
+  end
+end
