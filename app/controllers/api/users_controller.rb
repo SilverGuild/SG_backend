5f4e31971519ac5287beb5853c38007050403a41
elysableu <elysabward@gmail.com>
2025-07-25 17:18:34 -0600

    Fixing ci issue

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,6 @@
-# class Api::V1::UsersController < ApplicationController
-# end
+class Api::V1::UsersController < ApplicationController
+
+  def index()
+    return true
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+  root 'home#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index]
+    end
+  end
 end
