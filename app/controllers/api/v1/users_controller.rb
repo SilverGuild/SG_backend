529865997a0d578d9b7f0f5425f13b41317642d5
elysableu <elysabward@gmail.com>
2025-08-29 16:14:00 -0600

    Added indexing endpoint to users controller and created custom serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::UsersController < ApplicationController
 
-  def index()
-    return true
+  def index
+    users = User.all
+
+    render json: UserSerializer.new(users).serializable_hash
+  end
+
+  private
+
+  def allow_params
+    params.permit(:id, :username, :email)
   end
 end
