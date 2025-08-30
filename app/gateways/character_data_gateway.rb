16809f323d6d196e74a20a040308fbcd915deca1
elysableu <elysabward@gmail.com>
2025-08-30 01:04:19 -0600

    Add request methods to retrieve both character races and languages

--- app/gateways/character_data_gateway.rb
+++ app/gateways/character_data_gateway.rb
@@ -10,11 +10,23 @@ class CharacterDataGateway
   end
 
   def self.fetch_races
+    conn = connect()
+    response = conn.get("api/2014/races/")
 
+    json = JSON.parse(response.body, symbolize_names: true)
+    races = json[:results]
+
+    return races
   end
 
   def self.fetch_languages
+    conn = connect()
+    response = conn.get("api/2014/languages/")
 
+    json = JSON.parse(response.body, symbolize_names: true)
+    languages = json[:results]
+
+    return languages
   end
 
   private
