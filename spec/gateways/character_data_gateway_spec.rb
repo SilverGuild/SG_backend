dba62dbf70eadd4381021743c54bd58ad24e0f14
elysableu <elysabward@gmail.com>
2025-08-30 00:19:11 -0600

    Add test for character class request

--- spec/gateways/character_data_gateway_spec.rb
+++ spec/gateways/character_data_gateway_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe CharacterDataGateway do
 
   describe "GET /api/2014/classes/" do
     it "should make a request to D&D 5e API and return all character classes" do
-      
+      VCR.usse_cassette("character_classes") do
+
+        character_classes = CharacterDataGateway.classes
+
+        expect(character_classes).not_to be_empty
+        
+        # Test char class poro for specific information
+      end
     end
   end
 end
