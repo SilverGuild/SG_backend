52c951e65c9cbadd1fbbfdec05646e3ba9c4c282
Elysa Ward <116769731+elysableu@users.noreply.github.com>
2025-09-11 15:06:15 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root "home#index"
 
   namespace :api do
     namespace :v1 do
