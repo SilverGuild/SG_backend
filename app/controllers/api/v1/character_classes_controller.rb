2d3f362b174e5bf13f9292eb5c42b8f42c645f3f
elysableu <elysabward@gmail.com>
2025-09-18 17:01:41 -0600

    Add index method to handle get /api/v1/characteClass index endpoint

--- app/controllers/api/v1/character_classes_controller.rb
+++ app/controllers/api/v1/character_classes_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::CharacterClassesController < ApplicationController
-  
+  def index
+    character_classes = CharacterClass.all
+
+    render json: CharacterClassSerializer.new(character_classes).serializable_hash
+  end
 end
