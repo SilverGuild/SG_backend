e26c0699d25b351c1b7fbe64ea53911b77608ffc
elysableu <elysabward@gmail.com>
2025-09-18 17:15:11 -0600

    Update UsersSerializer to use SerializationHelper methods

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,16 +1,7 @@
 class UserSerializer
   include JSONAPI::Serializer
+  include SerializationHelpers
 
   attributes  :username,
               :email
-
-  # Override serializable_hash to confirm id is int type when rendered
-  def serializable_hash
-    hash = super
-
-    hash[:data] = [ hash[:data] ] unless hash[:data].is_a?(Array)
-    hash[:data].each { |item| item[:id] = item[:id].to_i }
-
-    hash
-  end
 end
