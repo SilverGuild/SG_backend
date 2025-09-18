b5a8d56bb11deb846ca06783159b1f6216cc377d
elysableu <elysabward@gmail.com>
2025-09-18 17:06:10 -0600

    Add get /api/v1/characteClass show endpoint

--- app/controllers/api/v1/character_classes_controller.rb
+++ app/controllers/api/v1/character_classes_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::CharacterClassesController < ApplicationController
 
     render json: CharacterClassSerializer.new(character_classes).serializable_hash
   end
+
+  def show
+    character_class = CharacterClass.find(params[:id])
+
+    render json: CharacterClassSerializer.new(character_class).serializable_hash
+  end
 end
