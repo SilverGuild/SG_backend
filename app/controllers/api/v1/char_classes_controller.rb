77b738679007aa073b2f98cdd14d6cfcbab63db1
elysableu <elysabward@gmail.com>
2025-09-18 15:02:46 -0600

    Add base files and sspec for charClasses controller

---
+++ app/controllers/api/v1/char_classes_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::CharClassesController < ApplicationController
+
+end
---
+++ spec/requests/api/v1/char_classes_controller.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe "charClasses endpoints" do
+  describe "RESTful endpoints" do
+    before(:each) do
+      # @class1 = User.create!(name: "Sorcerer", description: "", hit_die: "6", primary_ability: "", saving_throw_proficiencies: "")
+    end
+
+    describe "GET /api/v1/charClasses" do
+      xit "should retrieve all users" do
+
+      end
+    end
+
+    describe "GET /api/v1/users/{ID}" do
+      xit "should retrieve one user" do
+
+      end
+    end
+
+    describe "POST /api/v1/users" do
+     xit "should create a new user and return 201 Created status" do
+
+      end
+    end
+
+    describe "PATCH /api/v1/users/{ID}" do
+      xit "should updaate a user entry in the db and return successful status" do
+
+      end
+    end
+
+    describe "DELETE /api/v1/users/{ID}" do
+      xit "should destroy a user by id and return an empty response body" do
+
+      end
+    end
+  end
+end
