b2128efa49a070d5ff415dc32d1a31c14023f2b1
elysableu <elysabward@gmail.com>
2025-09-19 16:16:06 -0600

    Update routes with races show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [ :index, :show, :create, :update, :destroy ]
       resources :character_classes, only: [ :index, :show ]
+      resources :races, only: [ :index, :show]
     end
   end
 end
