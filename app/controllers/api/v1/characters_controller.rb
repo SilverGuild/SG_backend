07b1db865f192f80aa65943db85ebaa16a06d583
elysableu <elysabward@gmail.com>
2025-09-24 14:53:41 -0600

    Add index method to characters_Controller

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::CharactersController < ApplicationController
+  def index
+    characters = Character.all
 
+    render json: CharacterSerializer.new(characters).serializable_hash
+  end
 end
