5a62cdb4b6273c3a965cce44915e09af262c72d8
elysableu <elysabward@gmail.com>
2025-09-24 14:55:16 -0600

    Add show endpoint to characters_Controller

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::CharactersController < ApplicationController
 
     render json: CharacterSerializer.new(characters).serializable_hash
   end
+
+  def show
+    character = Character.find(params[:id])
+
+    render json: CharacterSerializer.new(character).serializable_hash
+  end
 end
