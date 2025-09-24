055a8ff778fab5b74e11a720885c832e6122e8c8
elysableu <elysabward@gmail.com>
2025-09-24 15:17:16 -0600

    Add validations and relationships to races model

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,2 +1,19 @@
 class Race < ApplicationRecord
+  # Many-to-many with characters
+  has_many :characters_races
+  has_many :races, through: :characters_races
+
+  # One-to-many with subraces
+
+  # Validate attributes
+  validates :name, presence: true
+  validates :desccription, presence: true
+  validates :speed, presence: true
+  validates :size, presence: true
+  validates :ability_bonuses, presence: true
+  validates :age_description, presence: true
+  validates :alignment_description, presence: true
+  validates :size_description, presence: true
+  validates :languages_description, presence: true
+  validates :languages, presence: true
 end
