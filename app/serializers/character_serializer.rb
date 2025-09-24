bddcfedb0366c7889b61789fe0b53cb807e63990
elysableu <elysabward@gmail.com>
2025-09-24 14:53:59 -0600

    Update character_serializer

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,5 +1,11 @@
-class CharacterClassSerializer
+class CharacterSerializer
   include JSONAPI::Serializer
   include SerializationHelpers
 
+  attributes  :name,
+              :level,
+              :experience_points,
+              :alignment,
+              :background,
+              :user_id
 end
