faac876582ee6c87e4d6d5e164eeaeb229e8ca0c
elysableu <elysabward@gmail.com>
2025-09-25 14:47:04 -0600

    men

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,18 @@
 class Character < ApplicationRecord
+  # Many-to-many with races
+  has_many :characters_races
+  has_many :races, through: :characters_races
+  # Many-to-many with classes
+  has_many :character_char_classes
+  has_many :classes, through: :characters_char_classes
+  # One-to-many with users
+  belongs_to :user
+
+  # Attribute validations
+  validates :name, presence: true
+  validates :level, presence: true
+  validates :experience_points, presence: true
+  validates :alignment, presence: true
+  validates :background, presence: true
+  validates :user_id, presence: true
 end
