2763a22e7d2210d8bbfe18314624e67a0bee3d5d
elysableu <elysabward@gmail.com>
2025-10-03 16:39:25 -0600

    Add spec test for each show endpoint for classes, races, and languages

--- spec/gateways/dnd5e_data_gateway_spec.rb
+++ spec/gateways/dnd5e_data_gateway_spec.rb
@@ -13,6 +13,16 @@ RSpec.describe Dnd5eDataGateway do
         expect(character_classes.last[:index]).to eq("wizard")
       end
     end
+
+     it "should make a request to D&D 5e API and return a single character class by id" do
+      VCR.use_cassette("single_character_class") do
+        character_class = Dnd5eDataGateway.fetch_dnd_data("classes", "sorcerer")
+
+        expect(character_class).not_to be_empty
+
+        expect(character_class[:index]).to eq("sorcerer")
+      end
+    end
   end
 
   describe "GET /api/2014/races/" do
@@ -26,10 +36,21 @@ RSpec.describe Dnd5eDataGateway do
         expect(character_races.last[:index]).to eq("tiefling")
       end
     end
+
+    it "should make a request to D&D 5e API and return a single character race by id" do
+      VCR.use_cassette("single_character_race") do
+        target_id = "halfling"
+        character_race = Dnd5eDataGateway.fetch_dnd_data("races", target_id)
+
+        expect(character_race).not_to be_empty
+
+        expect(character_race[:index]).to eq(target_id)
+      end
+    end
   end
 
   describe "GET /api/2014/languages/" do
-    it "should make a request to D&D 5e API and return all character races" do
+    it "should make a request to D&D 5e API and return all languages" do
       VCR.use_cassette("languages") do
         languages = Dnd5eDataGateway.fetch_dnd_data("languages")
 
@@ -37,6 +58,17 @@ RSpec.describe Dnd5eDataGateway do
         expect(languages.count).to eq(16)
         expect(languages.first[:index]).to eq("abyssal")
         expect(languages.last[:index]).to eq("undercommon")
+      end
+    end
+
+     it "should make a request to D&D 5e API and return a single language by id" do
+      VCR.use_cassette("single_character_race") do
+        target_id = "celestial"
+        language = Dnd5eDataGateway.fetch_dnd_data("languages", target_id)
+
+        expect(language).not_to be_empty
+
+        expect(language[:index]).to eq(target_id)
       end
     end
   end
