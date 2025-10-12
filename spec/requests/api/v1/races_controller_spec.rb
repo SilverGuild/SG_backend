e8f607ce328bec9f589e0ffbbb16276e154e2c22
elysableu <elysabward@gmail.com>
2025-10-12 13:56:13 -0600

    Refactored races_controller_spec to pull directly from the gateway rather than the db

--- spec/requests/api/v1/races_controller_spec.rb
+++ spec/requests/api/v1/races_controller_spec.rb
@@ -3,160 +3,157 @@ require "rails_helper"
 RSpec.describe "races endpoints", type: :request do
     describe "RESTful endpoints" do
         before(:each) do
-            @race1 = Race.create!(name: "Tiefling",
-                                    description: "My favroite character race to play",
-                                    speed: 30,
-                                    size: "medium",
-                                    ability_bonuses: [
-                                        {
-                                            "skill_name" => "int",
-                                            "bonus" => 1
-                                        },
-                                        {
-                                            "skill_name" => "cha",
-                                            "bonus" => 2
-                                        }
-                                    ],
-                                    age_description: "Tieflings mature at the same rate as humans but live a few years longer.",
-                                    alignment_description: "Tieflings might not have an innate tendency toward evil, but many of them end up there. Evil or not, an independent nature inclines many tieflings toward a chaotic alignment.",
-                                    size_description: "Tieflings are about the same size and build as humans. Your size is Medium.",
-                                    languages_description: "You can speak, read, and write Common and Infernal.",
-                                    languages: [
-                                        {
-                                            "language_name" => "common"
-                                        },
-                                        {
-                                            "language_name" => "infernal"
-                                        }
-                                    ]
-                                )
-            @race2 = Race.create!(name: "Dwarf",
-                                    description: "Small but mighty",
-                                    speed: 25,
-                                    size: "medium",
-                                    ability_bonuses: [
-                                        {
-                                            "skill_name" => "con",
-                                            "bonus" => 2
-                                        }
-                                    ],
-                                    age_description: "Dwarves mature at the same rate as humans, but they're considered young until they reach the age of 50. On average, they live about 350 years.",
-                                    alignment_description: "Most dwarves are lawful, believing firmly in the benefits of a well-ordered society. They tend toward good as well, with a strong sense of fair play and a belief that everyone deserves to share in the benefits of a just order.",
-                                    size_description: "Dwarves stand between 4 and 5 feet tall and average about 150 pounds. Your size is Medium.",
-                                    languages_description: "You can speak, read, and write Common and Dwarvish. Dwarvish is full of hard consonants and guttural sounds, and those characteristics spill over into whatever other language a dwarf might speak.",
-                                    languages: [
-                                        {
-                                            "language_name" => "common"
-                                        }, {
-                                            "language_name" => "dwarvish"
-                                        }
-                                    ]
-                                )
+            @dragonborn = { 
+                name: "Dragonborn",
+                description: "Big lizard!",
+                speed: 30,
+                size: "medium",
+                ability_bonuses: [
+                    {
+                        "skill_name" => "str",
+                        "bonus" => 2
+                    },
+                    {
+                        "skill_name" => "cha",
+                        "bonus" => 1
+                    }
+                ],
+                age_description: "Young dragonborn grow quickly. They walk hours after hatching, attain the size and development of a 10-year-old human child by the age of 3, and reach adulthood by 15. They live to be around 80.",
+                alignment_description: "Dragonborn tend to extremes, making a conscious choice for one side or the other in the cosmic war between good and evil. Most dragonborn are good, but those who side with evil can be terrible villains.",
+                size_description: "Dragonborn are taller and heavier than humans, standing well over 6 feet tall and averaging almost 250 pounds. Your size is Medium.",
+                languages_description: "You can speak, read, and write Common and Draconic. Draconic is thought to be one of the oldest languages and is often used in the study of magic. The language sounds harsh to most other creatures and includes numerous hard consonants and sibilants.",
+                languages: [
+                    {
+                        "language_name" => "common"
+                    }, {
+                        "language_name" => "draconic"
+                    }
+                ]
+            }
+            
+            @tiefling = {   
+                name: "Tiefling",
+                description: "My favroite character race to play",
+                speed: 30,
+                size: "medium",
+                ability_bonuses: [
+                    {
+                        "skill_name" => "int",
+                        "bonus" => 1
+                    },
+                    {
+                        "skill_name" => "cha",
+                        "bonus" => 2
+                    }
+                ],
+                age_description: "Tieflings mature at the same rate as humans but live a few years longer.",
+                alignment_description: "Tieflings might not have an innate tendency toward evil, but many of them end up there. Evil or not, an independent nature inclines many tieflings toward a chaotic alignment.",
+                size_description: "Tieflings are about the same size and build as humans. Your size is Medium.",
+                languages_description: "You can speak, read, and write Common and Infernal.",
+                languages: [
+                    {
+                        "language_name" => "common"
+                    },
+                    {
+                        "language_name" => "infernal"
+                    }
+                ]
+            }
 
-            @race3 = Race.create!(name: "Human",
-                                    description: "He's simple but reliable",
-                                    speed: 30,
-                                    size: "medium",
-                                    ability_bonuses: [
-                                        {
-                                            "skill_name" => "str",
-                                            "bonus" => 1
-                                        },
-                                        {
-                                            "skill_name" => "dex",
-                                            "bonus" => 1
-                                        },
-                                        {
-                                            "skill_name" => "con",
-                                            "bonus" => 1
-                                        },
-                                        {
-                                            "skill_name" => "int",
-                                            "bonus" => 1
-                                        },
-                                        {
-                                            "skill_name" => "wis",
-                                            "bonus" => 1
-                                        },
-                                        {
-                                            "skill_name" => "cha",
-                                            "bonus" => 1
-                                        }
-                                    ],
-                                    age_description: "Humans reach adulthood in their late teens and live less than a century.",
-                                    alignment_description: "Humans tend toward no particular alignment. The best and the worst are found among them.",
-                                    size_description: "Humans vary widely in height and build, from barely 5 feet to well over 6 feet tall. Regardless of your position in that range, your size is Medium.",
-                                    languages_description: "You can speak, read, and write Common and one extra language of your choice. Humans typically learn the languages of other peoples they deal with, including obscure dialects. They are fond of sprinkling their speech with words borrowed from other tongues: Orc curses, Elvish musical expressions, Dwarvish military phrases, and so on.",
-                                    languages: [
-                                        {
-                                            "language_name" => "common"
-                                        }
-                                    ]
-                                )
-
-            @target_id = @race2.id
+            @half_orc = { 
+                name: "Half-orc",
+                description: "No one looks better with an axe!",
+                speed: 30,
+                size: "medium",
+                ability_bonuses: [
+                    {
+                        "skill_name" => "str",
+                        "bonus" => 2
+                    },
+                    {
+                        "skill_name" => "con",
+                        "bonus" => 1
+                    }
+                ],
+                age_description: "Half-orcs mature a little faster than humans, reaching adulthood around age 14. They age noticeably faster and rarely live longer than 75 years.",
+                alignment_description: "Half-orcs inherit a tendency toward chaos from their orc parents and are not strongly inclined toward good. Half-orcs raised among orcs and willing to live out their lives among them are usually evil.",
+                size_description: "Half-orcs are somewhat larger and bulkier than humans, and they range from 5 to well over 6 feet tall. Your size is Medium.",
+                languages_description: "You can speak, read, and write Common and Orc. Orc is a harsh, grating language with hard consonants. It has no script of its own but is written in the Dwarvish script.",
+                languages: [
+                    {
+                        "language_name" => "common"
+                    }, {
+                        "language_name" => "orc"
+                    }
+                ]
+            }
         end
 
         describe "GET /api/v1/races" do
-            it "should retrieve all character races" do
-                get "/api/v1/races"
-                expect(response).to be_successful
+            it "should retrieve all character races directly from dnd5eapi" do
+                VCR.use_cassette("character_classes") do
+                    get "/api/v1/races"
+                    expect(response).to be_successful
 
-                json = JSON.parse(response.body, symbolize_names: true)
+                    json = JSON.parse(response.body, symbolize_names: true)
 
-                races = json[:data]
-                expect(races.count).to eq(3)
+                    races = json[:data]
+                    expect(races.count).to eq(3)
 
-                first_race = races.first
-                last_race = races.last
+                    first_race = races.first
+                    last_race = races.last
 
-                expect(first_race[:id]).to eq(@race1.id)
-                expect(first_race[:attributes][:name]).to eq(@race1.name)
-                expect(first_race[:attributes][:description]).to eq(@race1.description)
-                expect(first_race[:attributes][:speed]).to eq(@race1.speed)
-                expect(first_race[:attributes][:size]).to eq(@race1.size)
-                expect(first_race[:attributes][:ability_bonuses].first[:skill_name]).to eq(@race1.ability_bonuses.first["skill_name"])
-                expect(first_race[:attributes][:age_description]).to eq(@race1.age_description)
-                expect(first_race[:attributes][:alignment_description]).to eq(@race1.alignment_description)
-                expect(first_race[:attributes][:size_description]).to eq(@race1.size_description)
-                expect(first_race[:attributes][:languages_description]).to eq(@race1.languages_description)
-                expect(first_race[:attributes][:languages].first[:language_name]).to eq(@race1.languages.first["language_name"])
+                    expect(first_race).to be_an_instance_of RacePoro
+                    expect(first_race.name).to eq(@dragonborn[:name])
+                    expect(first_race.description).to eq(@dragonborn[:description])
+                    expect(first_race.speed).to eq(@dragonborn[:speed])
+                    expect(first_race.size).to eq(@dragonborn[:size])
+                    expect(first_race.ability_bonuses.first[:skill_name]).to eq(@dragonborn[:ability_bonuses].first["skill_name"])
+                    expect(first_race.age_description).to eq(@dragonborn[:age_description])
+                    expect(first_race.alignment_description).to eq(@dragonborn[:alignment_description])
+                    expect(first_race.size_description).to eq(@dragonborn[:size_description])
+                    expect(first_race.languages_description).to eq(@dragonborn[:languages_description])
+                    expect(first_race.languages.first[:language_name]).to eq(@dragonborn[:languages].first["language_name"])
 
-                expect(last_race[:id]).to eq(@race3.id)
-                expect(last_race[:attributes][:name]).to eq(@race3.name)
-                expect(last_race[:attributes][:description]).to eq(@race3.description)
-                expect(last_race[:attributes][:speed]).to eq(@race3.speed)
-                expect(last_race[:attributes][:size]).to eq(@race3.size)
-                expect(last_race[:attributes][:ability_bonuses].first[:skill_name]).to eq(@race3.ability_bonuses.first["skill_name"])
-                expect(last_race[:attributes][:age_description]).to eq(@race3.age_description)
-                expect(last_race[:attributes][:alignment_description]).to eq(@race3.alignment_description)
-                expect(last_race[:attributes][:size_description]).to eq(@race3.size_description)
-                expect(last_race[:attributes][:languages_description]).to eq(@race3.languages_description)
-                expect(last_race[:attributes][:languages].first[:language_name]).to eq(@race3.languages.first["language_name"])
+                    expect(last_race).to be_an_instance_of RacePoro
+                    expect(last_race.name).to eq(@tiefling[:name])
+                    expect(last_race.description).to eq(@tiefling[:description])
+                    expect(last_race.speed).to eq(@tiefling[:speed])
+                    expect(last_race.size).to eq(@tiefling[:size])
+                    expect(last_race.ability_bonuses.first[:skill_name]).to eq(@tiefling[:ability_bonuses].first["skill_name"])
+                    expect(last_race.age_description).to eq(@tiefling[:age_description])
+                    expect(last_race.alignment_description).to eq(@tiefling[:alignment_description])
+                    expect(last_race.size_description).to eq(@tiefling[:size_description])
+                    expect(last_race.languages_description).to eq(@tiefling[:languages_description])
+                    expect(last_race.languages.first[:language_name]).to eq(@tiefling[:languages].first["language_name"])
+                end
             end
         end
 
         describe "GET /api/v1/races/{ID}" do
             it "should retrieve one character race" do
-                get "/api/v1/races/#{@target_id}"
-                expect(response).to be_successful
+                VCR.use_cassette("single_character_race_half_orc") do
+                    target_id = "half-orc"
+                    get "/api/v1/races/#{target_id}"
 
-                json = JSON.parse(response.body, symbolize_names: true)
+                    expect(response).to be_successful
 
-                target = json[:data].first
+                    json = JSON.parse(response.body, symbolize_names: true)
 
-                expect(target[:id]).to eq(@race2.id)
-                expect(target[:attributes][:name]).to eq(@race2.name)
-                expect(target[:attributes][:description]).to eq(@race2.description)
-                expect(target[:attributes][:speed]).to eq(@race2.speed)
-                expect(target[:attributes][:size]).to eq(@race2.size)
-                expect(target[:attributes][:ability_bonuses].first[:skill_name]).to eq(@race2.ability_bonuses.first["skill_name"])
-                expect(target[:attributes][:age_description]).to eq(@race2.age_description)
-                expect(target[:attributes][:alignment_description]).to eq(@race2.alignment_description)
-                expect(target[:attributes][:size_description]).to eq(@race2.size_description)
-                expect(target[:attributes][:languages_description]).to eq(@race2.languages_description)
-                expect(target[:attributes][:languages].first[:language_name]).to eq(@race2.languages.first["language_name"])
-            end
+                    target = json[:data].first
+                
+                    expect(target).to be_an_instance_of RacePoro
+                    expect(target.name).to eq(@half_orc[:name])
+                    expect(target.description).to eq(@half_orc[:description])
+                    expect(target.speed).to eq(@half_orc[:speed])
+                    expect(target.size).to eq(@half_orc[:size])
+                    expect(target.ability_bonuses.first[:skill_name]).to eq(@half_orc[:ability_bonuses].first["skill_name"])
+                    expect(target.age_description).to eq(@half_orc[:age_description])
+                    expect(target.alignment_description).to eq(@half_orc[:alignment_description])
+                    expect(target.size_description).to eq(@half_orc[:size_description])
+                    expect(target.languages_description).to eq(@half_orc[:languages_description])
+                    expect(target.languages.first[:language_name]).to eq(@half_orc[:languages].first["language_name"])
+                end
         end
     end
 end
