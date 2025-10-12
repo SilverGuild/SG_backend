0773e7ae10fd8b6d74cbae364a9b1c83d79a7d4a
elysableu <elysabward@gmail.com>
2025-10-12 15:49:15 -0600

    Add id optional assignment to fetch methods

--- app/gateways/dnd5e_data_gateway.rb
+++ app/gateways/dnd5e_data_gateway.rb
@@ -1,17 +1,17 @@
 class Dnd5eDataGateway
-  def self.fetch_character_classes(id)
+  def self.fetch_character_classes(id = "")
     fetch_dnd_data("classes", id)
   end
 
-  def self.fetch_races(id)
-    fetch_dnd_data("races", id)
+  def self.fetch_races(id = "")
+    fetch_dnd_data("races", id) 
   end
 
-  def self.fetch_langauges(id)
+  def self.fetch_langauges(id = "")
     fetch_dnd_data("languages", id)
   end
 
-  def self.fetch_dnd_data(category, id = "")
+  def self.fetch_dnd_data(category, id)
     conn = connect()
     endpoint = id.empty? ? "api/2014/#{category}" : "api/2014/#{category}/#{id}"
     response = conn.get(endpoint)
