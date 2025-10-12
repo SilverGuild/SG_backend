3202de05387a4f3ce0905ee4549b21528651f401
elysableu <elysabward@gmail.com>
2025-10-12 16:04:20 -0600

    Update RacePoro to address either a list of races or a single detailed race

--- app/poros/race_poro.rb
+++ app/poros/race_poro.rb
@@ -1,27 +1,32 @@
 class RacePoro
   include PoroTransformations
 
-  attr_reader :name,
-              :description,
+  attr_reader :id,
+              :name,
+              :url,
+              # :description,
               :speed,
               :size,
               :ability_bonuses,
               :age_description,
               :alignment_description,
               :size_description,
-              :languages_description,
+              :language_description,
               :languages
 
   def initialize(race_data)
+    # require "pry"; binding.pry
+    @id                     = race_data[:index]&.downcase
     @name                   = race_data[:name]
-    @description            = race_data[:description]
+    @url                    = race_data[:url]
+    # @description            = race_data[:description]
     @speed                  = race_data[:speed]
     @size                   = race_data[:size]
-    @ability_bonuses        = json_to_array(race_data[:ability_bonuses])
-    @age_description        = race_data[:age_description]
-    @alignment_description  = race_data[:alignment_description]
+    @ability_bonuses        = race_data[:ability_bonuses] == nil ? race_data[:ability_bonuses] : json_to_array(race_data[:ability_bonuses])
+    @age_description        = race_data[:age]
+    @alignment_description  = race_data[:alignment]
     @size_description       = race_data[:size_description]
-    @languages_description  = race_data[:languages_description]
+    @language_description   = race_data[:language_desc]
     @languages              = json_to_array(race_data[:languages])
   end
 end
