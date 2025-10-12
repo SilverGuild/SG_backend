47f7e69cec40266329431e720c2d25439eddcd7d
elysableu <elysabward@gmail.com>
2025-10-12 16:05:10 -0600

    Update race_serializer to set :id, :url, and DETAILED_ATTRIBUTES

--- app/serializers/race_serializer.rb
+++ app/serializers/race_serializer.rb
@@ -1,15 +1,36 @@
 class RaceSerializer
   include JSONAPI::Serializer
-  include SerializationHelpers
 
-  attributes  :name,
-              :description,
+  set_id do |race|
+    race.id
+  end
+
+  attribute :name do |race|
+    race.name
+  end
+
+  attribute :url, if: Proc.new { |record, params|
+    params && params[:detailed] == false
+  } do |race|
+    race.url
+  end
+
+  DETAILED_ATTRIBUTES = [ 
+              # :description,
               :speed,
               :size,
               :ability_bonuses,
               :age_description,
               :alignment_description,
               :size_description,
-              :languages_description,
+              :language_description,
               :languages
+  ]
+  
+  DETAILED_ATTRIBUTES.each do |attr|
+    attribute attr, if: Proc.new { |record, params|
+      params && params[:detailed] == true
+    }
+  end
+              
 end
