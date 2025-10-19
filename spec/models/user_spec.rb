6d2a4d439b6dec476b939c7f5c2fe1bfdc7e4684
elysableu <elysabward@gmail.com>
2025-10-19 15:55:23 -0600

    Added tests for user model validations and relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "happy paths" do
+    before(:each) do
+       @user =  @user1 = User.create!(username: "user1", email: "user1@gmail.com")
+
+       @character = Character.create!(name: "Kaelynn Thornwick",
+                                      level: 3,
+                                      experience_points: 320,
+                                      alignment: "Neutral Neutral",
+                                      background: "Artisan",
+                                      user_id: @user.id,
+                                      character_class_name: "rogue",
+                                      race_name: "half-elf")
+    end
+
+    describe "relationships" do
+      it { should have_many(:characters) }
+    end
+
+    describe "validations" do
+      it { should validate_presence_of(:username) }
+      it { should validate_presence_of(:email) }
+    end
+  end
 end
