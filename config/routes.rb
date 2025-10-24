1073ff905a696457ce984827e5d7f09560b390fa
elysableu <elysabward@gmail.com>
2025-10-24 00:04:19 -0600

    Add subclasses route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :character_classes, only: [ :index, :show ]
+      resources :subclasses, only: [ :index, :show ]
       resources :races, only: [ :index, :show ]
       resources :languages, only: [ :index, :show ]
 
