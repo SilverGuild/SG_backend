66dd213604d065749c6bb58e7e9c723132769a04
elysableu <elysabward@gmail.com>
2025-10-24 19:24:06 -0600

    Set up subclass serializer

--- app/serializers/subclass_serializer.rb
+++ app/serializers/subclass_serializer.rb
@@ -1,4 +1,29 @@
 class SubclassSerializer
   include JSONAPI::Serializer
 
+  set_id do |subclass|
+    subclass.id
+  end
+
+  attribute :name do |subclass|
+    subclass.name
+  end
+
+  attribute :url, if: Proc.new { |record, params|
+    params && params[:detailed] == false
+  } do |subclass|
+    subclass.url
+  end
+
+  DETAILED_ATTRIBUTES = [
+    :class_id,
+    :description,
+    :flavor
+  ]
+
+  DETAILED_ATTRIBUTES.each do |attr|
+    attribute attr, if: Proc.new { |record, params|
+      params && params[:detailed] == true
+    }
+  end
 end
