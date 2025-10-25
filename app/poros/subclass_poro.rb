a4d504b8878b040646612aec685121995c0f2648
elysableu <elysabward@gmail.com>
2025-10-24 19:23:47 -0600

    Set up subclass PORO

--- app/poros/subclass_poro.rb
+++ app/poros/subclass_poro.rb
@@ -1,3 +1,19 @@
 class SubclassPoro
-  
+  include PoroTransformations
+
+  attr_reader :id,
+              :class_id,
+              :name,
+              :flavor,
+              :description,
+              :url
+
+  def initialize(subclass_data)
+    @id           = subclass_data[:index]
+    @class_id     = subclass_data.dig(:class, :index)
+    @name         = subclass_data[:name]
+    @flavor       = subclass_data[:subclass_flavor]
+    @description  = subclass_data[:desc]&.first
+    @url          = subclass_data[:url]
+  end
 end
