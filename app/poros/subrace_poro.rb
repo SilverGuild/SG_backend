28665d10be5597444de7dd8347d634380048bfdb
elysableu <elysabward@gmail.com>
2025-10-24 20:58:09 -0600

    Refactor race_id assignment

--- app/poros/subrace_poro.rb
+++ app/poros/subrace_poro.rb
@@ -9,9 +9,10 @@ class SubracePoro
               :url
 
   def initialize(subrace_data)
+    # require "pry"; binding.pry
     @id               = subrace_data[:index]
     @name             = subrace_data[:name]
-    @race_id          = subrace_data[:race][:index]
+    @race_id          = subrace_data.dig(:race, :index)
     @description      = subrace_data[:desc]
     @ability_bonuses  = subrace_data[:ability_bonuses] == nil ? subrace_data[:ability_bonuses] : json_to_array(subrace_data[:ability_bonuses])
     @url              = subrace_data[:url]
