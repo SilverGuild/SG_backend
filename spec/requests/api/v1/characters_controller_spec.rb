f6e715a62d3d8fa6d4de5a149cd3ef5a951333dc
elysableu <elysabward@gmail.com>
2025-10-26 15:16:43 -0600

    Add languages to test characters

--- spec/requests/api/v1/characters_controller_spec.rb
+++ spec/requests/api/v1/characters_controller_spec.rb
@@ -12,7 +12,10 @@ RSpec.describe "API::V1::Characters", type: :request do
                                       background: "Hermit",
                                       user_id: @user.id,
                                       character_class_id: "druid",
-                                      race_id: "gnome")
+                                      race_id: "gnome",
+                                      subclass_id: "land",
+                                      subrace_id: "rock-gnome",
+                                      languges: [ "common", "gnomish" ])
       @character2 = Character.create!(name: "Theren Nightblade",
                                       level: 5,
                                       experience_points: 500,
@@ -20,7 +23,10 @@ RSpec.describe "API::V1::Characters", type: :request do
                                       background: "Aristocrate",
                                       user_id: @user.id,
                                       character_class_id: "paladin",
-                                      race_id: "dragonborn")
+                                      race_id: "dragonborn",
+                                      subclass_id: "devotion",
+                                      subrace_id: "",
+                                      languges: [ "common", "draconic" ])
       @character3 = Character.create!(name: "Mira Stormhaven",
                                       level: 8,
                                       experience_points: 853,
@@ -28,8 +34,10 @@ RSpec.describe "API::V1::Characters", type: :request do
                                       background: "Acolyte",
                                       user_id: @user.id,
                                       character_class_id: "fighter",
-                                      race_id: "halfling")
-
+                                      race_id: "halfling",
+                                      subclass_id: "champion",
+                                      subrace_id: "lightfoot-halfling",
+                                      languges: [ "common", "halfling" ])
       @character4 = Character.create!(name: "Thalorin Emberwick",
                                       level: 7,
                                       experience_points: 755,
@@ -37,7 +45,10 @@ RSpec.describe "API::V1::Characters", type: :request do
                                       background: "Sage",
                                       user_id: @user2.id,
                                       character_class_name: "cleric",
-                                      race_name: "half-elf")
+                                      race_name: "half-elf",
+                                      subclass_id: "life",
+                                      subrace_id: "",
+                                      languges: [ "common", "elvish" ])
 
       @target_id = @character2.id
     end
@@ -62,6 +73,11 @@ RSpec.describe "API::V1::Characters", type: :request do
         expect(first_character[:attributes][:alignment]).to eq(@character1[:alignment])
         expect(first_character[:attributes][:background]).to eq(@character1[:background])
         expect(first_character[:attributes][:user_id]).to eq(@character1[:user_id])
+        expect(first_character[:attributes][:character_class_id]).to eq(@character1[:character_class_id])
+        expect(first_character[:attributes][:race_id]).to eq(@character1[:race_id])
+        expect(first_character[:attributes][:subclass_id]).to eq(@character1[:subclass_id])
+        expect(first_character[:attributes][:subrace_id]).to eq(@character1[:subrace_id])
+        expect(first_character[:attributes][:languages]).to eq(@character1[:languages])
 
         expect(last_character[:id]).to eq(@character4[:id])
         expect(last_character[:attributes][:name]).to eq(@character4[:name])
@@ -70,6 +86,12 @@ RSpec.describe "API::V1::Characters", type: :request do
         expect(last_character[:attributes][:alignment]).to eq(@character4[:alignment])
         expect(last_character[:attributes][:background]).to eq(@character4[:background])
         expect(last_character[:attributes][:user_id]).to eq(@character4[:user_id])
+        expect(last_character[:attributes][:character_class_id]).to eq(@character4[:character_class_id])
+        expect(last_character[:attributes][:race_id]).to eq(@character4[:race_id])
+        expect(last_character[:attributes][:subclass_id]).to eq(@character4[:subclass_id])
+        expect(last_character[:attributes][:subrace_id]).to eq(@character4[:subrace_id])
+        expect(last_character[:attributes][:languages]).to eq(@character4[:languages])
+
       end
     end
 
@@ -89,6 +111,11 @@ RSpec.describe "API::V1::Characters", type: :request do
         expect(target[:attributes][:alignment]).to eq(@character2[:alignment])
         expect(target[:attributes][:background]).to eq(@character2[:background])
         expect(target[:attributes][:user_id]).to eq(@character2[:user_id])
+        expect(target[:attributes][:character_class_id]).to eq(@character2[:character_class_id])
+        expect(target[:attributes][:race_id]).to eq(@character2[:race_id])
+        expect(target[:attributes][:subclass_id]).to eq(@character2[:subclass_id])
+        expect(target[:attributes][:subrace_id]).to eq(@character2[:subrace_id])
+        expect(target[:attributes][:languages]).to eq(@character2[:languages])
       end
     end
 
@@ -102,7 +129,12 @@ RSpec.describe "API::V1::Characters", type: :request do
           experience_points: 621,
           alignment: "Lawful Evil",
           background: "Aristocrate",
-          user_id: @user.id
+          user_id: @user.id,
+          character_class_id: "paladin",
+          race_id: "dragonborn",
+          subclass_id: "devotion",
+          subrace_id: "",
+          languges: [ "common", "draconic" ]
         }
 
         patch "/api/v1/characters/#{@target_id}", params: updated_params
@@ -113,26 +145,26 @@ RSpec.describe "API::V1::Characters", type: :request do
 
         target = json[:data].first
 
-        expect(target[:id]).to eq(@character2[:id])
+        expect(target[:id]).to eq(character[:id])
         expect(target[:attributes][:name]).to eq(character[:name])
         expect(target[:attributes][:level]).to eq(updated_params[:level])
         expect(target[:attributes][:experience_points]).to eq(updated_params[:experience_points])
         expect(target[:attributes][:alignment]).to eq(character[:alignment])
         expect(target[:attributes][:background]).to eq(character[:background])
         expect(target[:attributes][:user_id]).to eq(character[:user_id])
+        expect(target[:attributes][:character_class_id]).to eq(character[:character_class_id])
+        expect(target[:attributes][:race_id]).to eq(character[:race_id])
+        expect(target[:attributes][:subclass_id]).to eq(character[:subclass_id])
+        expect(target[:attributes][:subrace_id]).to eq(character[:subrace_id])
+        expect(target[:attributes][:languages]).to eq(character[:languages])
 
-        # Verify db has updated
+        # Verify db has updated specific attributes
         character.reload
-        expect(target[:id]).to eq(character[:id])
-        expect(target[:attributes][:name]).to eq(character[:name])
-        expect(target[:attributes][:level]).to eq(character[:level])
-        expect(target[:attributes][:experience_points]).to eq(character[:experience_points])
-        expect(target[:attributes][:alignment]).to eq(character[:alignment])
-        expect(target[:attributes][:background]).to eq(character[:background])
-        expect(target[:attributes][:user_id]).to eq(character[:user_id])
+        expect(character[:attributes][:level]).to eq(updated_params[:level])
+        expect(character[:attributes][:experience_points]).to eq(updated_params[:experience_points])
       end
     end
-
+    
     describe "DELETE /api/v1/characters/:id" do
       it "should destroy a character by id and return an empty response body" do
         expect {
