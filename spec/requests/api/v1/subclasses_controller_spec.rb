26787ae9aa49de07903418d4e0144764dfa74837
elysableu <elysabward@gmail.com>
2025-11-01 16:48:27 -0600

    Add spec test for subclasses_controller when show endpoint returns 404 status

--- spec/requests/api/v1/subclasses_controller_spec.rb
+++ spec/requests/api/v1/subclasses_controller_spec.rb
@@ -30,22 +30,37 @@ RSpec.describe "API::V1::Subclasses", type: :request do
     end
 
     describe "GET /api/v1/subclasses/:id" do
-      it "should retrieve one subclass by id from dnd5e api" do
-        VCR.use_cassette("subclasses_fiend") do
-          target_id = "fiend"
-          get "/api/v1/subclasses/#{target_id}"
+      describe "happy paths" do
+        it "should retrieve one subclass by id from dnd5e api" do
+          VCR.use_cassette("subclasses_fiend") do
+            target_id = "fiend"
+            get "/api/v1/subclasses/#{target_id}"
 
-          expect(response).to be_successful
+            expect(response).to be_successful
 
-          json = JSON.parse(response.body, symbolize_names: true)
-          target = json[:data]
+            json = JSON.parse(response.body, symbolize_names: true)
+            target = json[:data]
 
-          expect(target[:type]).to eq("subclass")
-          expect(target[:id]).to eq("fiend")
-          expect(target[:attributes][:name]).to eq("Fiend")
-          expect(target[:attributes][:flavor]).to eq("Otherworldly Patron")
-          expect(target[:attributes][:class_id]).to eq("warlock")
-          expect(target[:attributes][:description]).to eq("You have made a pact with a fiend from the lower planes of existence, a being whose aims are evil, even if you strive against those aims. Such beings desire the corruption or destruction of all things, ultimately including you. Fiends powerful enough to forge a pact include demon lords such as Demogorgon, Orcus, Fraz'Urb-luu, and Baphomet; archdevils such as Asmodeus, Dispater, Mephistopheles, and Belial; pit fiends and balors that are especially mighty; and ultroloths and other lords of the yugoloths.")
+            expect(target[:type]).to eq("subclass")
+            expect(target[:id]).to eq("fiend")
+            expect(target[:attributes][:name]).to eq("Fiend")
+            expect(target[:attributes][:flavor]).to eq("Otherworldly Patron")
+            expect(target[:attributes][:class_id]).to eq("warlock")
+            expect(target[:attributes][:description]).to eq("You have made a pact with a fiend from the lower planes of existence, a being whose aims are evil, even if you strive against those aims. Such beings desire the corruption or destruction of all things, ultimately including you. Fiends powerful enough to forge a pact include demon lords such as Demogorgon, Orcus, Fraz'Urb-luu, and Baphomet; archdevils such as Asmodeus, Dispater, Mephistopheles, and Belial; pit fiends and balors that are especially mighty; and ultroloths and other lords of the yugoloths.")
+          end
+        end
+      end
+
+      describe "sad paths" do
+        before do
+          stub_request(:get, "https://www.dnd5eapi.co/api/2014/subclasses/test").to_return(status: 404, body: { error: "Not found" }.to_json)
+        end
+
+        it "returns a 404 status when target race does not exist" do
+          get "/api/v1/subclasses/test"
+
+          expect(response).to have_http_status(:not_found)
+          expect(JSON.parse(response.body)).to include("error" => "Subclass not found")
         end
       end
     end
