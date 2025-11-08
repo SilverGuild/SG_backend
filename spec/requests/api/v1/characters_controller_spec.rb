03a37365eac4e73432d8611e25c341817759ec82
elysableu <elysabward@gmail.com>
2025-11-08 14:01:32 -0700

    Set up base for sad path testing

--- spec/requests/api/v1/characters_controller_spec.rb
+++ spec/requests/api/v1/characters_controller_spec.rb
@@ -54,126 +54,219 @@ RSpec.describe "API::V1::Characters", type: :request do
     end
 
     describe "GET /api/v1/characters" do
-      it "should retrieve all characters" do
-        get "/api/v1/characters"
-        expect(response).to be_successful
+      context "happy paths" do
+        it "should retrieve all characters" do
+          get "/api/v1/characters"
+          expect(response).to be_successful
 
-        json = JSON.parse(response.body, symbolize_names: true)
+          json = JSON.parse(response.body, symbolize_names: true)
 
-        characters = json[:data]
-        expect(characters.count).to eq(4)
+          characters = json[:data]
+          expect(characters.count).to eq(4)
 
-        first_character = characters.first
-        last_character = characters.last
+          first_character = characters.first
+          last_character = characters.last
 
-        expect(first_character[:id]).to eq(@character1[:id])
-        expect(first_character[:attributes][:name]).to eq(@character1[:name])
-        expect(first_character[:attributes][:level]).to eq(@character1[:level])
-        expect(first_character[:attributes][:experience_points]).to eq(@character1[:experience_points])
-        expect(first_character[:attributes][:alignment]).to eq(@character1[:alignment])
-        expect(first_character[:attributes][:background]).to eq(@character1[:background])
-        expect(first_character[:attributes][:user_id]).to eq(@character1[:user_id])
-        expect(first_character[:attributes][:character_class_id]).to eq(@character1[:character_class_id])
-        expect(first_character[:attributes][:race_id]).to eq(@character1[:race_id])
-        expect(first_character[:attributes][:subclass_id]).to eq(@character1[:subclass_id])
-        expect(first_character[:attributes][:subrace_id]).to eq(@character1[:subrace_id])
-        expect(first_character[:attributes][:languages]).to eq(@character1[:languages])
+          expect(first_character[:id]).to eq(@character1[:id])
+          expect(first_character[:attributes][:name]).to eq(@character1[:name])
+          expect(first_character[:attributes][:level]).to eq(@character1[:level])
+          expect(first_character[:attributes][:experience_points]).to eq(@character1[:experience_points])
+          expect(first_character[:attributes][:alignment]).to eq(@character1[:alignment])
+          expect(first_character[:attributes][:background]).to eq(@character1[:background])
+          expect(first_character[:attributes][:user_id]).to eq(@character1[:user_id])
+          expect(first_character[:attributes][:character_class_id]).to eq(@character1[:character_class_id])
+          expect(first_character[:attributes][:race_id]).to eq(@character1[:race_id])
+          expect(first_character[:attributes][:subclass_id]).to eq(@character1[:subclass_id])
+          expect(first_character[:attributes][:subrace_id]).to eq(@character1[:subrace_id])
+          expect(first_character[:attributes][:languages]).to eq(@character1[:languages])
 
-        expect(last_character[:id]).to eq(@character4[:id])
-        expect(last_character[:attributes][:name]).to eq(@character4[:name])
-        expect(last_character[:attributes][:level]).to eq(@character4[:level])
-        expect(last_character[:attributes][:experience_points]).to eq(@character4[:experience_points])
-        expect(last_character[:attributes][:alignment]).to eq(@character4[:alignment])
-        expect(last_character[:attributes][:background]).to eq(@character4[:background])
-        expect(last_character[:attributes][:user_id]).to eq(@character4[:user_id])
-        expect(last_character[:attributes][:character_class_id]).to eq(@character4[:character_class_id])
-        expect(last_character[:attributes][:race_id]).to eq(@character4[:race_id])
-        expect(last_character[:attributes][:subclass_id]).to eq(@character4[:subclass_id])
-        expect(last_character[:attributes][:subrace_id]).to eq(@character4[:subrace_id])
-        expect(last_character[:attributes][:languages]).to eq(@character4[:languages])
+          expect(last_character[:id]).to eq(@character4[:id])
+          expect(last_character[:attributes][:name]).to eq(@character4[:name])
+          expect(last_character[:attributes][:level]).to eq(@character4[:level])
+          expect(last_character[:attributes][:experience_points]).to eq(@character4[:experience_points])
+          expect(last_character[:attributes][:alignment]).to eq(@character4[:alignment])
+          expect(last_character[:attributes][:background]).to eq(@character4[:background])
+          expect(last_character[:attributes][:user_id]).to eq(@character4[:user_id])
+          expect(last_character[:attributes][:character_class_id]).to eq(@character4[:character_class_id])
+          expect(last_character[:attributes][:race_id]).to eq(@character4[:race_id])
+          expect(last_character[:attributes][:subclass_id]).to eq(@character4[:subclass_id])
+          expect(last_character[:attributes][:subrace_id]).to eq(@character4[:subrace_id])
+          expect(last_character[:attributes][:languages]).to eq(@character4[:languages])
+        end
+      end
+
+      context "sad paths" do
+        # Will be added late P1 with authentication and autherization overhaul **********
+        # it "returns a 401 status when user is not authenticated" do
+
+        # end
+
+        # it "returns a 401 status when user (non-owner) does not have dungeon master access" do
+
+        # end
+        # ***********
       end
     end
 
-     describe "GET /api/v1/characters:id}" do
-      it "should retrieve one character" do
-        get "/api/v1/characters/#{@target_id}"
-        expect(response).to be_successful
+    describe "GET /api/v1/characters:id}" do
+      context "happy paths" do
+        it "should retrieve one character" do
+          get "/api/v1/characters/#{@target_id}"
+          expect(response).to be_successful
 
-        json = JSON.parse(response.body, symbolize_names: true)
+          json = JSON.parse(response.body, symbolize_names: true)
 
-        target = json[:data].first
+          target = json[:data].first
 
-        expect(target[:id]).to eq(@character2[:id])
-        expect(target[:attributes][:name]).to eq(@character2[:name])
-        expect(target[:attributes][:level]).to eq(@character2[:level])
-        expect(target[:attributes][:experience_points]).to eq(@character2[:experience_points])
-        expect(target[:attributes][:alignment]).to eq(@character2[:alignment])
-        expect(target[:attributes][:background]).to eq(@character2[:background])
-        expect(target[:attributes][:user_id]).to eq(@character2[:user_id])
-        expect(target[:attributes][:character_class_id]).to eq(@character2[:character_class_id])
-        expect(target[:attributes][:race_id]).to eq(@character2[:race_id])
-        expect(target[:attributes][:subclass_id]).to eq(@character2[:subclass_id])
-        expect(target[:attributes][:subrace_id]).to eq(@character2[:subrace_id])
-        expect(target[:attributes][:languages]).to eq(@character2[:languages])
+          expect(target[:id]).to eq(@character2[:id])
+          expect(target[:attributes][:name]).to eq(@character2[:name])
+          expect(target[:attributes][:level]).to eq(@character2[:level])
+          expect(target[:attributes][:experience_points]).to eq(@character2[:experience_points])
+          expect(target[:attributes][:alignment]).to eq(@character2[:alignment])
+          expect(target[:attributes][:background]).to eq(@character2[:background])
+          expect(target[:attributes][:user_id]).to eq(@character2[:user_id])
+          expect(target[:attributes][:character_class_id]).to eq(@character2[:character_class_id])
+          expect(target[:attributes][:race_id]).to eq(@character2[:race_id])
+          expect(target[:attributes][:subclass_id]).to eq(@character2[:subclass_id])
+          expect(target[:attributes][:subrace_id]).to eq(@character2[:subrace_id])
+          expect(target[:attributes][:languages]).to eq(@character2[:languages])
+        end
+      end
+
+      context "sad paths" do
+        it "returns a 400 status when character ID is invalid format" do
+
+        end
+
+        # Will be added late P1 with authentication and autherization overhaul **********
+        # it "returns a 401 status when user is not authenticated" do
+
+        # end
+
+        # it "returns a 401 status when user (non-owner) does not have dungeon master access" do
+
+        # end
+        # ***********
+
+        it "returns a 404 status when target character is not found" do
+
+        end
       end
     end
 
     describe "PATCH /api/v1/characters/:id" do
-      it "should updaate a character entry in the db and return successful status" do
-        character = Character.find(@target_id)
+      context "happy paths" do
+        it "should updaate a character entry in the db and return successful status" do
+          character = Character.find(@target_id)
 
-        updated_params = {
-          name: "Theren Nightblade",
-          level: 6,
-          experience_points: 621,
-          alignment: "Lawful Evil",
-          background: "Aristocrate",
-          user_id: @user.id,
-          character_class_id: "paladin",
-          race_id: "dragonborn",
-          subclass_id: "devotion",
-          subrace_id: "",
-          languages: [ "common", "draconic" ]
-        }
+          updated_params = {
+            name: "Theren Nightblade",
+            level: 6,
+            experience_points: 621,
+            alignment: "Lawful Evil",
+            background: "Aristocrate",
+            user_id: @user.id,
+            character_class_id: "paladin",
+            race_id: "dragonborn",
+            subclass_id: "devotion",
+            subrace_id: "",
+            languages: [ "common", "draconic" ]
+          }
 
-        patch "/api/v1/characters/#{@target_id}", params: updated_params
+          patch "/api/v1/characters/#{@target_id}", params: updated_params
 
-        expect(response).to be_successful
+          expect(response).to be_successful
 
-        json = JSON.parse(response.body, symbolize_names: true)
+          json = JSON.parse(response.body, symbolize_names: true)
 
-        target = json[:data].first
+          target = json[:data].first
 
-        expect(target[:id]).to eq(character[:id])
-        expect(target[:attributes][:name]).to eq(character[:name])
-        expect(target[:attributes][:level]).to eq(updated_params[:level])
-        expect(target[:attributes][:experience_points]).to eq(updated_params[:experience_points])
-        expect(target[:attributes][:alignment]).to eq(character[:alignment])
-        expect(target[:attributes][:background]).to eq(character[:background])
-        expect(target[:attributes][:user_id]).to eq(character[:user_id])
-        expect(target[:attributes][:character_class_id]).to eq(character[:character_class_id])
-        expect(target[:attributes][:race_id]).to eq(character[:race_id])
-        expect(target[:attributes][:subclass_id]).to eq(character[:subclass_id])
-        expect(target[:attributes][:subrace_id]).to eq(character[:subrace_id])
-        expect(target[:attributes][:languages]).to eq(character[:languages])
+          expect(target[:id]).to eq(character[:id])
+          expect(target[:attributes][:name]).to eq(character[:name])
+          expect(target[:attributes][:level]).to eq(updated_params[:level])
+          expect(target[:attributes][:experience_points]).to eq(updated_params[:experience_points])
+          expect(target[:attributes][:alignment]).to eq(character[:alignment])
+          expect(target[:attributes][:background]).to eq(character[:background])
+          expect(target[:attributes][:user_id]).to eq(character[:user_id])
+          expect(target[:attributes][:character_class_id]).to eq(character[:character_class_id])
+          expect(target[:attributes][:race_id]).to eq(character[:race_id])
+          expect(target[:attributes][:subclass_id]).to eq(character[:subclass_id])
+          expect(target[:attributes][:subrace_id]).to eq(character[:subrace_id])
+          expect(target[:attributes][:languages]).to eq(character[:languages])
 
-        # Verify db has updated specific attributes
-        character.reload
+          # Verify db has updated specific attributes
+          character.reload
 
-        expect(character[:level]).to eq(updated_params[:level])
-        expect(character[:experience_points]).to eq(updated_params[:experience_points])
+          expect(character[:level]).to eq(updated_params[:level])
+          expect(character[:experience_points]).to eq(updated_params[:experience_points])
+        end
+      end
+      
+      context "sad paths" do
+        it "returns a 400 status when character ID is invalid format" do
+
+        end
+
+        it "returns a 400 status when a parameter is missing" do
+
+        end
+
+        it "returns a 400 status when a parameter is invalid" do
+
+        end
+
+
+        # Will be added late P1 with authentication and autherization overhaul **********
+        # it "returns a 401 status when user is not authenticated" do
+
+        # end
+
+        # it "returns a 401 status when user (non-owner) does not have dungeon master access" do
+
+        # end
+        # ***********
+
+        it "returns a 404 status when target character is not found" do
+          
+        end
+
+        it "returns a 422 status when updating to a name that already exists" do
+
+        end
       end
     end
 
     describe "DELETE /api/v1/characters/:id" do
-      it "should destroy a character by id and return an empty response body" do
-        expect {
-          delete "/api/v1/characters/#{@target_id}"
-        }.to change(Character, :count).by(-1)
+      context "happy paths" do
+        it "should destroy a character by id and return an empty response body" do
+          expect {
+            delete "/api/v1/characters/#{@target_id}"
+          }.to change(Character, :count).by(-1)
 
-        expect(response).to be_successful
-        expect(response.body).to be_empty
-        expect(Character.exists?(@target_id)).to be false
+          expect(response).to be_successful
+          expect(response.body).to be_empty
+          expect(Character.exists?(@target_id)).to be false
+        end
+      end
+
+      context "sad paths" do
+        it "returns a 400 status when character ID is invalid format" do
+
+        end
+
+        # Will be added late P1 with authentication and autherization overhaul **********
+        # it "returns a 401 status when user is not authenticated" do
+
+        # end
+
+        # it "returns a 401 status when user (non-owner) does not have dungeon master access" do
+
+        # end
+        # ***********
+
+        it "returns a 404 status when target character is not found" do
+          
+        end
       end
     end
   end
