21dca4aabe21dc79cb42f5b7a066cbcb396dbbdc
elysableu <elysabward@gmail.com>
2026-07-16 20:54:12 -0600

    Update seed with assign_ability_scores for each character

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,20 +38,34 @@ BACKGROUNDS = %w[
   acolyte charlatan criminal entertainer folk hero guild artisan hermit noble outlander sage sailor soldier urchin
 ].freeze
 
+ABILITIES = %w[ str dex con int wis cha ].freeze
+
 SKILLS = %w[
   acrobatics animal-handling arcana athletics deception history
   insight intimidation investigation medicine nature perception
   performance persuasion religion sleight-of-hand stealth survival
 ].freeze
 
-def assign_random_skills(character)
+def assign_ability_scores(character)
+  saving_throw_ids = ABILITIES.sample(2)
+
+  ABILITIES.each do |ability_id|
+    character.ability_scores.create!(
+      ability_id: ability_id,
+      score: rand(8..18),
+      saving_throw_proficient: saving_throw_ids.include?(ability_id)
+    )
+  end
+end
+
+def assign_skills(character)
   proficient_skill_ids = SKILLS.sample(rand(2..4))
   expertise_skill_ids = rand < 0.3 ? proficient_skill_ids.sample(1) : []
 
   SKILLS.each do |skill_id|
     character.skills.create!(
       skill_id: skill_id,
-      proficient: true,
+      proficient: proficient_skill_ids.include?(skill_id),
       expertise: expertise_skill_ids.include?(skill_id)
     )
   end
@@ -97,7 +111,8 @@ User.all.each do |user|
       languages: RACES[race][:languages]
     )
 
-    assign_random_skills(character)
+    assign_ability_scores(character)
+    assign_skills(character)
   end
 end
 
@@ -136,5 +151,6 @@ Rails.logger.info "Create test characters for admin account"
     languages: RACES[race][:languages]
   )
 
-  assign_random_skills(character)
+  assign_ability_scores(character)
+  assign_skills(character)
 end
