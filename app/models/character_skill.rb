8914c8b0b335c9bef4992aa9e581f425e6101f00
elysableu <elysabward@gmail.com>
2026-07-18 15:51:57 -0600

    Add skill_id_mutable() to CharacterSkill model

--- app/models/character_skill.rb
+++ app/models/character_skill.rb
@@ -5,9 +5,15 @@ class CharacterSkill < ApplicationRecord
   validates :skill_id, format: { with: /\A[a-z-]+\z/, message: "is invalid" }, allow_blank: true
   validates :skill_id, uniqueness: { scope: :character_id }
 
+  validate :skill_id_immutable, on: :update
+
   validate :expertise_requires_proficiency
 
   private
+  
+    def skill_id_immutable
+      errors.add(:skill_id, "can't be changed after creation") if skill_id_changed?
+    end
 
   def expertise_requires_proficiency
     if expertise? && !proficient?
