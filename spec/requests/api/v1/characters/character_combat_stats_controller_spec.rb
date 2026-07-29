84b138004982cd59eaec858b6e67ee9d0a778159
elysableu <elysabward@gmail.com>
2026-07-29 11:44:01 -0600

    Minor fixes

--- spec/requests/api/v1/characters/character_combat_stats_controller_spec.rb
+++ spec/requests/api/v1/characters/character_combat_stats_controller_spec.rb
@@ -1,4 +1,4 @@
-requires "rails_helper"
+require "rails_helper"
 
 RSpec.describe "API::V1::CharacterCombatStats", type: :request do
   let(:password) {"password123"}
@@ -76,7 +76,7 @@ RSpec.describe "API::V1::CharacterCombatStats", type: :request do
         get "/api/v1/characters/#{@character2.id}/combat_stats"
 
         expect(response).to have_http_status(:not_found)
-        expect(JSON.parse(response.body)).to include("error" => "Combat Stat not found")
+        expect(JSON.parse(response.body)).to include("error" => "Combat Stats not found")
       end
     end
   end
@@ -86,7 +86,7 @@ RSpec.describe "API::V1::CharacterCombatStats", type: :request do
       it "should create combat stats for a specific character and return 201 Created status" do
         test_params = { current_hp: 20, max_hp: 20, temporary_hp: 0, hit_dice_remaining: 5, death_save_successes: 0, death_save_failures: 0, stable: false, armor_class: 14, conditions: [] }
 
-        post "api/v1/characters/#{@character2.id}/combat_stats", params: { character_combat_stat: test_params }, as: :json
+        post "/api/v1/characters/#{@character2.id}/combat_stats", params: { character_combat_stat: test_params }, as: :json
 
         expect(response).to have_http_status(:created)
 
@@ -112,14 +112,14 @@ RSpec.describe "API::V1::CharacterCombatStats", type: :request do
         expect(JSON.parse(response.body)).to include("error" => "Invalid character ID")
       end
 
-      shared_examples "returns a 400 for invalid parameter" do |param, invalid_value, error_message|
+      shared_examples "returns 400 for invalid parameter" do |param, invalid_value, error_message|
         it "returns 400 status when #{param} is #{invalid_value.inspect}" do
           text_params = { current_hp: 20, max_hp: 20, temporary_hp: 0, hit_dice_remaining: 5, death_save_successes: 0, death_save_failures: 0, stable: false, armor_class: 14, conditions: [] }.merge(param => invalid_value)
 
           post "/api/v1/characters/#{@character2.id}/combat_stats", params: { character_combat_stat: text_params }, as: :json
 
           expect(response).to have_http_status(:bad_request)
-          expect(JSON.parse(response.body).to include("error" => error_message))
+          expect(JSON.parse(response.body)).to include("error" => error_message)
         end
       end
 
@@ -137,7 +137,7 @@ RSpec.describe "API::V1::CharacterCombatStats", type: :request do
         it_behaves_like "returns 400 for invalid parameter", :current_hp, 999, "Current hp can't exceed max hp"
         it_behaves_like "returns 400 for invalid parameter", :hit_dice_remaining, 6, "Hit dice remaining can't exceed character level"
         it_behaves_like "returns 400 for invalid parameter", :death_save_successes, 4, "Death save successes must be less than or equal to 3"
-        it_behaves_like "returns 400 for invalid parameter", :conditions, [ "Poisoned!" ], "Conditions contains an invalid slug"
+        it_behaves_like "returns 400 for invalid parameter", :conditions, [ "Poisoned!" ], "Conditions contains an invalid condition slug"
       end
 
       xit "returns a 401 status when user is not authenticated" do
