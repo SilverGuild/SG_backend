67f52530aa51d990b5276e47a566143c21b22e8b
elysableu <elysabward@gmail.com>
2026-08-27 15:14:39 -0600

    Supress logger output for asset requests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,12 +61,6 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
   # Annotate rendered view with file names.
   config.action_view.annotate_rendered_view_with_filenames = true
 
